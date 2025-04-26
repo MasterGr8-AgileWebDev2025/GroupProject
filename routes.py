6356dbe4ce67d2da7390f30d0d3af46bdd5e935a
briellewang <24386828@student.uwa.edu.au>
2025-04-26 16:09:50 +0800

    Update routes.py(init part)

--- routes.py
+++ routes.py
@@ -1,8 +1,10 @@
-from app import app
+from routes.main_routes import *
+from routes.match_routes import *
 
-
-# Home page
-@app.route('/')
-def index():
-    from datetime import datetime
-    return render_template('index.html', now=datetime.now())
+def register_with_app(app):
+    """
+    Legacy function for backward compatibility.
+    This will be removed in the future.
+    """
+    from routes import register_blueprints
+    register_blueprints(app)
