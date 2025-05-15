bba8e36603cf50acb4c68b7d28d8558c4f1a1bbd
Daniel Xu <2290408+danielxu1987@users.noreply.github.com>
2025-05-15 19:03:47 +0800

    Fixed the error of element not interactable after click()

--- selenium_tests/test_homepage.py
+++ selenium_tests/test_homepage.py
@@ -8,15 +8,17 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 # waiting time for elements to load
-waitingTime = 2
+waitingTime = 1.5
 
 options = webdriver.ChromeOptions()
-# options.add_argument('--headless=new') # For Chrome 109+
+options.add_argument('--headless=new') # For Chrome 109+
+options.add_argument('--start-maximized') # without maximising the window error message appears
 
 service = Service(executable_path="chromedriver.exe")
 
 def setup():
-    driver = webdriver.Chrome(options=options, service=service) 
+    driver = webdriver.Chrome(options=options, service=service)
+    driver.implicitly_wait(waitingTime)
     try:
         driver.get("http://127.0.0.1:5000/")
         print("Driver title: " + driver.title)
@@ -30,7 +32,6 @@ def setup():
         return driver
 
 def teardown(driver):
-    time.sleep(waitingTime)
     driver.quit()
     print("Browser closed.")
 
@@ -48,8 +49,10 @@ def test_signup_button():
         assert button_register.text.lower() in "sign up", "Sign Up button is not present on the page"
         print("Sign Up button is present on the homepage.")
 
+        # driver.implicitly_wait(waitingTime)
         button_register.click()
         assert driver.current_url.split('/')[-1] == "register", "Clicking on signup does not open registering page."
+        print("Sign Up button is clicked.")
 
         form_register = WebDriverWait(driver, waitingTime).until(
             EC.presence_of_element_located(
@@ -57,7 +60,7 @@ def test_signup_button():
             )
         )
         assert form_register.is_displayed(), "Registration form is not present on the page"
-        print(f"Registration form is present on page {driver.current_url}.")
+        print(f"The registering page is open on {driver.current_url} and the registration form is present on.")
 
     except Exception as e:
         print(f"TEST FAILED: {str(e)}")
@@ -81,6 +84,7 @@ def test_login_button():
 
         button_login.click()
         assert driver.current_url.split('/')[-1] == "login", "Clicking on login button does not open login page."
+        print(f"Login button is clicked and login page shows up on {driver.current_url}")
 
     except Exception as e:
         print(f"TEST FAILED: {str(e)}")
@@ -91,7 +95,6 @@ def test_login_button():
 def test_explanation_display():
     try:
         driver = setup()
-        driver.implicitly_wait(waitingTime)
         
         # Check if upload, analyze and improve exist in the page
         upload = driver.find_element(By.XPATH, '//h3[contains(text(), "1. Upload")]')
